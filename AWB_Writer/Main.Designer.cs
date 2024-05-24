6df53ec415e09c96ece3108d741122d3c6af4e49
HD Texture Packs <admin@hdpacks.com>
2024-05-24 08:25:29 -0400

    First commit
    First commit

---
+++ AWB_Writer/Main.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace AWB_Writer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnOpenInput = new System.Windows.Forms.Button();
+            this.btnOpenOutput = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtInput.Location = new System.Drawing.Point(25, 26);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(467, 44);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "Folder Containing Input Files";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtOutput.Location = new System.Drawing.Point(25, 87);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(467, 44);
+            this.txtOutput.TabIndex = 1;
+            this.txtOutput.Text = "Path To Save AWB File";
+            // 
+            // btnOpenInput
+            // 
+            this.btnOpenInput.Location = new System.Drawing.Point(519, 26);
+            this.btnOpenInput.Name = "btnOpenInput";
+            this.btnOpenInput.Size = new System.Drawing.Size(135, 60);
+            this.btnOpenInput.TabIndex = 2;
+            this.btnOpenInput.Text = "Open";
+            this.btnOpenInput.UseVisualStyleBackColor = true;
+            this.btnOpenInput.Click += new System.EventHandler(this.btnOpenInput_Click);
+            // 
+            // btnOpenOutput
+            // 
+            this.btnOpenOutput.Location = new System.Drawing.Point(519, 87);
+            this.btnOpenOutput.Name = "btnOpenOutput";
+            this.btnOpenOutput.Size = new System.Drawing.Size(135, 60);
+            this.btnOpenOutput.TabIndex = 3;
+            this.btnOpenOutput.Text = "Open";
+            this.btnOpenOutput.UseVisualStyleBackColor = true;
+            this.btnOpenOutput.Click += new System.EventHandler(this.btnOpenOutput_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(25, 157);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(218, 60);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(252, 157);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(218, 60);
+            this.btnQuit.TabIndex = 5;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 229);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpenOutput);
+            this.Controls.Add(this.btnOpenInput);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmMain";
+            this.Text = "AWB Writer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnOpenInput;
+        private System.Windows.Forms.Button btnOpenOutput;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnQuit;
+    }
+}
+
---
+++ AWB_Writer/Main.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Library.IO;
+
+namespace AWB_Writer
+{    
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnOpenInput_Click(object sender, EventArgs e)
+        {
+            var folderOpen = new FolderBrowserDialog();
+            folderOpen.Description = "Select path to the folder containing the files you want to import.";
+            folderOpen.ShowDialog();
+            txtInput.Text = folderOpen.SelectedPath;
+        }
+
+        private void btnOpenOutput_Click(object sender, EventArgs e)
+        {
+            var folderSave = new SaveFileDialog();
+            folderSave.Title = "Select path to save the AWB file.";
+            folderSave.DefaultExt = ".awb";
+            folderSave.Filter = "AWB File (*.awb)|*.awb";
+            folderSave.ShowDialog();
+            txtOutput.Text = folderSave.FileName;
+        }
+
+        private int nullCalc(int size)
+        {
+            int mod = size % 32;
+            if (mod == 0)
+                return 0;
+            else
+                return 32 - mod;
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!Path.IsPathRooted(txtInput.Text) || !Path.IsPathRooted(txtOutput.Text))
+                MessageBox.Show("Please enter a valid paths.");
+            else
+            {
+                DirectoryInfo dir = new DirectoryInfo(txtInput.Text);
+                var files = dir.GetFiles("*.*", SearchOption.TopDirectoryOnly);
+                MemoryStream stream = new MemoryStream();
+                var writer = new FileWriter(stream);
+                writer.ByteOrder = Syroot.BinaryData.ByteOrder.LittleEndian;
+                writer.WriteSignature("AFS2");
+                writer.Write(263170);
+                writer.Write(files.Length);
+                writer.Write(32);
+                for (int i = 0; i < files.Length; i++)
+                    writer.Write(i);
+
+                int[] offsetArr = new int[files.Length + 1];                
+                int offset = (int)writer.Position + files.Length * 4 + 4;
+                int lastMod = nullCalc(offset);
+                offsetArr[0] = offset + lastMod;
+                writer.Write(offset);
+
+                int count = 1;
+                foreach (FileInfo file in files)
+                {                    
+                    offset += (int)file.Length + lastMod;
+                    lastMod = nullCalc(offset);
+                    offsetArr[count] = offset + lastMod;
+                    writer.Write(offset);
+                    count++;
+                }
+
+                count = 0;
+                foreach (FileInfo file in files)
+                {
+                    byte[] buffer = File.ReadAllBytes(file.FullName);
+                    writer.SeekBegin(offsetArr[count]);
+                    writer.Write(buffer);
+                    count++;
+                }
+                File.WriteAllBytes(txtOutput.Text, stream.ToArray());
+                stream.Dispose();
+                MessageBox.Show("Finished!");
+            }
+        }
+    }
+}
